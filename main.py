0482f2d12a6ff68228b0465921f320f6425205c0
unknown <art.guilher@gmail.com>
2025-03-13 21:38:13 -0300

    aula final

---
+++ main.py
@@ -0,0 +1,15 @@
+import requests 
+
+reqUrl = "https://api.github.com/octocat"
+
+headerList = { 
+    "Accept": "application/vnd.github+json",
+    "User-Agent": "Thunder Client(https://thunderclient.com)",
+    "X-GitHub-Api-Version": "2022-11-28"
+}
+
+payload = ""
+
+response = requests.request("GET", reqUrl, data=payload, headers=headerList)
+
+print(response.text)
